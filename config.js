bf57f664d925923cd492ecc3fbb97e5741fbe492
Laurenz <git@laure.nz>
2023-04-26 23:13:04 +0200

    config

--- config.js
+++ config.js
@@ -1,4 +1,6 @@
 module.exports = {
+    username: 'renovate',
+    gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     repositories: ['lalten/renovate-rules-python-pip-compile'],
     allowedPostUpgradeCommands: ['^bazel run //:requirements.update$'],
 };
