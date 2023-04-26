d5954604234ac6c7ba16c1c433acfe5926431da1
Laurenz <git@laure.nz>
2023-04-26 23:28:56 +0200

    onboarding default

--- config.js
+++ config.js
@@ -2,5 +2,9 @@ module.exports = {
     username: 'renovate',
     gitAuthor: 'Renovate Bot <bot@renovateapp.com>',
     repositories: ['lalten/renovate-rules-python-pip-compile'],
+    onboarding: {
+        "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+        extends: ["config:base"]
+    },
     allowedPostUpgradeCommands: ['^bazel run //:requirements.update$'],
 };
